564fcaf5e30ba1231ae7cd1d065fd7da9693a491
khaivern <khaivern@agmostudio.com>
2023-01-16 11:10:48 +0800

    fix proj struct

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ButtonModule, CarouselModule, FormModule } from '@coreui/angular';
-import { FooterComponent } from '../components/footer/footer.component';
-import { HeaderComponent } from '../components/header/header.component';
-import { HeroComponent } from '../components/hero/hero.component';
-import { DeleteService } from '../components/products/item/delete.service';
-import { ItemComponent } from '../components/products/item/item.component';
-import { ProductsComponent } from '../components/products/products.component';
-import { LandingComponent } from '../pages/landing/landing.component';
-import { ProductDetailComponent } from '../pages/product-detail/product-detail.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { DeleteService } from '../../components/products/item/delete.service';
+import { ItemComponent } from '../../components/products/item/item.component';
+import { ProductsComponent } from '../../components/products/products.component';
+import { LandingComponent } from '../../pages/landing/landing.component';
+import { ProductDetailComponent } from '../../pages/product-detail/product-detail.component';
 import { HomeComponent } from './home.component';
 
 @NgModule({
